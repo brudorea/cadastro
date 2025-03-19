cebfa5e97c229c15bef7f61192856e970c02e133
BRUNO HENRIQUE DOREA DE LIMA <bruno.hdlima@senacsp.edu.br>
2025-03-18 22:00:45 -0300

    Criação do formulário para atividade avaliativa do Prof

---
+++ src/views/renderer.js
@@ -0,0 +1,28 @@
+/**
+ * Processo de renderização do documento index.html
+ */
+
+console.log("Processo de renderização")
+
+// inserção da data no rodapé
+function obterData() {
+    const data = new Date()
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return data.toLocaleDateString('pt-BR', options)
+}
+
+document.getElementById('dataAtual').innerHTML = obterData()
+
+api.dbStatus((event, message) => {
+    console.log(message)
+    if (message === "conectado") {
+        document.getElementById('iconeDB').src = "../public/img/dbon.png"
+    } else {
+        document.getElementById('iconeDB').src = "../public/img/dboff.png"    
+    }
+})
